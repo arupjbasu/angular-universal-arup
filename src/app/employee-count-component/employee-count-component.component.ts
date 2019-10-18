a446ffb1954669caa6fa44c355873791408ff3fa
arupjbasu <arupjbasu@gmail.com>
2019-10-18 10:23:01 +0530

    3 files modified

--- src/app/employee-count-component/employee-count-component.component.ts
+++ src/app/employee-count-component/employee-count-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input, Output,EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'employee-count',
@@ -6,9 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-count-component.component.css']
 })
 export class EmployeeCountComponent implements OnInit {
-   all: number = 10;
-   male: number = 5;
-   female: number = 5;
+    @Input()  all: number ;//= 10;
+    @Input() male: number ;//= 5;
+    @Input() female: number ;//= 5;
+     // Holds the selected value of the radio button
+    selectedRadioButtonValue: string = 'All';
+    @Output()
+    countRadioButtonSelectionChanged: EventEmitter<string> =
+                                        new EventEmitter<string>();
+
+    
+    onRadioButtonSelectionChange() {
+        this.countRadioButtonSelectionChanged
+            .emit(this.selectedRadioButtonValue);
+    }
   constructor() { }
 
   ngOnInit() {
--- src/app/employee-list-component/employee-list-component.component.ts
+++ src/app/employee-list-component/employee-list-component.component.ts
@@ -11,8 +11,7 @@ export class EmployeeListComponent {
     constructor() {
         this.employees = [
             {
-                code: 'emp101', name: 'Tom', gender: 'Male',
-                annualSalary: 5500, dateOfBirth: '6/25/1988'
+                code: 'emp101', name: 'Tom', gender: 'Male',annualSalary: 5500, dateOfBirth: '6/25/1988'
             },
             {
                 code: 'emp102', name: 'Alex', gender: 'Male',
@@ -30,6 +29,25 @@ export class EmployeeListComponent {
                 code: 'emp105', name: 'Nancy', gender: 'Female',
                 annualSalary: 6700.826, dateOfBirth: '12/15/1982'
             },
+            {
+                 code: 'emp106', name: 'Steve', gender: 'Male',
+                 annualSalary: 7700.481, dateOfBirth: '11/18/1979'
+            }
+
         ];
+
+        
+    }
+
+    getTotalEmployeesCount(): number {
+        return this.employees.length;
+    }
+
+    getMaleEmployeesCount(): number {
+        return this.employees.filter(e => e.gender === 'Male').length;
+    }
+
+    getFemaleEmployeesCount(): number { 
+        return this.employees.filter(e => e.gender === 'Female').length;
     }
 }
